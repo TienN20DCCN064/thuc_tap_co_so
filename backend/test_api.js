6f8d2bc52a8241d185e9e5f217b8202c3b1bebdd
hoducnhan <honhan3062002@gmail.com>
2025-07-19 16:14:57 +0700

    tesst git

--- backend/test_api.js
+++ backend/test_api.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "123456",
     database: "bong_da",
 });
 
